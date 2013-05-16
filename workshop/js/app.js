a0a93b04b0cb707c00d10205e399ef6cf0ccc235
Matthieu DUMAS <matthieu.dumas1@gmail.com>
2013-05-16 23:51:11 +0200

    Integrated fresh UI

--- workshop/js/app.js
+++ workshop/js/app.js
@@ -1,12 +1,12 @@
 /* App Module */
 
-angular.module("workshop",["filters"]).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/logs', {templateUrl: 'workshop/html/log-list.html',   controller: LogCtrl}).
-      when('/logs/:logId', {templateUrl: 'workshop/html/log-detail.html', controller: LogDetailCtrl}).
-      otherwise({redirectTo: '/logs'});
-}]);
+angular.module("workshop", ["filters"]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/logs', {templateUrl: 'workshop/html/log-list.html', controller: LogCtrl}).
+            when('/logs/:logId', {templateUrl: 'workshop/html/log-detail.html', controller: LogDetailCtrl}).
+            otherwise({redirectTo: '/logs'});
+    }]);
 
 
 /* Controllers */
@@ -17,42 +17,44 @@ function headerCtrl($scope) {
 
 function LogCtrl($scope, $http) {
 
-
-    $http.get('workshop/data/logs.json').success(function(data, status, headers, config) {
+    $http.get('workshop/data/logs.json').success(function (data, status, headers, config) {
         $scope.logs = data;
     });
 
-    $scope.selectedStatus={
-        "200":true,
-        "404":true,
-        "500":true
-    }
-
-
-
-    $scope.statusFilter = function(log) {
-
-        if ($scope.selectedStatus[log.status]) {
-            return true;
-        }
-
-        return false;
+    $scope.selectedStatus = {
+        "200": true,
+        "404": true,
+        "500": true
     };
 
+    $scope.selectedMethods = {
+        "GET": true,
+        "POST": true,
+        "PUT": true,
+        "DELETE": true
+    };
+
+    $scope.statusFilter = function (log) {
+        return $scope.selectedStatus[log.status];
+    };
+
+    $scope.methodsFilter = function (log) {
+        return $scope.selectedMethods[log.method];
+    };
 
 }
 
 function LogDetailCtrl($scope, $routeParams, $http) {
 
-
-    $http.get('workshop/data/logs.json').success(function(data, status, headers, config) {
+    $http.get('workshop/data/logs.json').success(function (data, status, headers, config) {
         for (var i = 0; i < data.length; i++) {
-            if(data[i].id==$routeParams.logId){
+            if (data[i].id == $routeParams.logId) {
                 $scope.log = data[i];
             }
         }
 
     });
+
 }
 
 
@@ -66,7 +68,7 @@ angular.module('filters', []).
                 return text;
             }
             else {
-                return String(text).substring(0, length-end.length) + end;
+                return String(text).substring(0, length - end.length) + end;
             }
 
         };
